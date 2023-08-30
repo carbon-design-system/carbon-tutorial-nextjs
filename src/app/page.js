85bac15c6a924b309be7f8ea5148ae1443a7f67a
Andrea N. Cardona <cardona.n.andrea@gmail.com>
2023-08-30 12:17:01 -0700

    test pr

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Please get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
