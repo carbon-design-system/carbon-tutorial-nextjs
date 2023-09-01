d81f4cc8bb8f61bce33b615ea51db4d676614210
Taylor Jones <taylor.jones826@gmail.com>
2023-09-01 09:38:50 -0500

    chore: testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-
 export default function Home() {
   return (
     <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+      Hello Carbon! Well, not quite yet. This is the starting point for the
+      Carbon NextJS tutorial. testing
     </div>
-
-  )
+  );
 }
