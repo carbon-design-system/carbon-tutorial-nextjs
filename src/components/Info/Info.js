5f639756730be61309b9637701178298bac9901d
Guilherme Datilio Ribeiro <guilhermedatilio@gmail.com>
2023-09-01 16:53:11 -0300

    fix: fix image size (#19)
    * fix: fix image size
    
    * fix: ran yarn format and build

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,9 +2,9 @@ import { Grid, Column } from '@carbon/react';
 
 // Take in a phrase and separate the third word in an array
 function createArrayFromPhrase(phrase) {
-    const splitPhrase = phrase.split(' ');
-    const thirdWord = splitPhrase.pop();
-    return [splitPhrase.join(' '), thirdWord];
+  const splitPhrase = phrase.split(' ');
+  const thirdWord = splitPhrase.pop();
+  return [splitPhrase.join(' '), thirdWord];
 }
 
 const InfoSection = (props) => (
@@ -17,18 +17,18 @@ const InfoSection = (props) => (
 );
 
 const InfoCard = (props) => {
-    const splitHeading = createArrayFromPhrase(props.heading);
-  
-    return (
-      <Column sm={4} md={8} lg={4} className="info-card">
-        <h4 className="info-card__heading">
-          {`${splitHeading[0]} `}
-          <strong>{splitHeading[1]}</strong>
-        </h4>
-        <p className="info-card__body">{props.body}</p>
-        {props.icon()}
-      </Column>
-    );
+  const splitHeading = createArrayFromPhrase(props.heading);
+
+  return (
+    <Column sm={4} md={8} lg={4} className="info-card">
+      <h4 className="info-card__heading">
+        {`${splitHeading[0]} `}
+        <strong>{splitHeading[1]}</strong>
+      </h4>
+      <p className="info-card__body">{props.body}</p>
+      {props.icon()}
+    </Column>
+  );
 };
-  
+
 export { InfoSection, InfoCard };
