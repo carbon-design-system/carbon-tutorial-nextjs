50d8eb4248c1424a16b5969e9ce5216e955bcaf2
Casey Gollan <casey@bullshit.systems>
2023-12-08 22:06:21 -0500

    feat(tutorial): complete step 1

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,13 +1,15 @@
 'use client';
 
 import TutorialHeader from '@/components/TutorialHeader/TutorialHeader';
-import { Content } from '@carbon/react';
+import { Content, Theme } from '@carbon/react';
 
 export function Providers({ children }) {
   return (
     <div>
-      <TutorialHeader />
-      <Content>{children}</Content>
+      <Theme theme="g100">
+        <TutorialHeader />
+        <Content>{children}</Content>
+      </Theme>
     </div>
   );
 }
