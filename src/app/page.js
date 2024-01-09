6faabb3a92ed10a9e79970fc86e137cafac38459
Paulo Augusto de Macena Pereira <paulo.augusto@ibm.com>
2024-01-09 16:28:23 -0300

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
+import LandingPage from './home/page';
 
-export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
-
-  )
+export default function Page() {
+  return <LandingPage />;
 }
