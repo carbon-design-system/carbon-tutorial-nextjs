a430b33d9717900a8addbe8fa7c6a6cb4deadcf7
Janne Lappalainen <jalap@88-115-41-253.elisa-laajakaista.fi>
2024-02-07 20:50:59 +0200

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+'use client';
+import { Button } from '@carbon/react';
+import LandingPage from './home/page';
 
-export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
-
-  )
+export default function Page() {
+  return <LandingPage />;
 }
+
+//export default function Home() {
+// return (
+// <Button>Button</Button>
+
+//)
+//}
