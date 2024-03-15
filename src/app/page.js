9530d04d119b2ec99a77c7d859c5b56ce93c8e8c
Henrique Borges De Almeida Filho <hbaf71@gmail.com>
2024-03-15 16:24:09 -0300

    removing comments

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,3 @@ import LandingPage from './home/page';
 export default function Page() {
   return <LandingPage />;
 }
-
-/* import LandingPage from './repos/page';
-
-export default function Page() {
-  return <LandingPage />;
-} */
