05e2a35edc69c2cb3824cae0cb9b60820ac7c5c4
Shweta Mandavgane <shweta.mandavgane@modivcare.com>
2024-04-25 16:39:24 -0400

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,6 @@
+'use client';
+import LandingPage from '@/app/home/page';
 
 export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
-
-  )
+  return <LandingPage />;
 }
