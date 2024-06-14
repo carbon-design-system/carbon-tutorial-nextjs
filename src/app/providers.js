9b8fa87679c9d260efb3649c5903716e5645259d
HarishV <harish@harishvs-mbp.in.ibm.com>
2024-06-14 13:47:42 +0530

    feat(tutorial): complete step 1

---
+++ src/app/providers.js
@@ -0,0 +1,15 @@
+'use client';
+
+import TutorialHeader from '@/components/TutorialHeader/TutorialHeader';
+import { Content, Theme } from '@carbon/react';
+
+export function Providers({ children }) {
+  return (
+    <div>
+      <Theme theme="g100">
+        <TutorialHeader />
+      </Theme>
+      <Content>{children}</Content>
+    </div>
+  );
+}
