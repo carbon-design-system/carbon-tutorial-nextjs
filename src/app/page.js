8e17ca90dbd9fe63d59648159210c38cb0ef8dca
Ivan Warlock <Ivan.Warlock@ibm.com>
2024-06-19 13:59:25 -0600

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
+'use client';
 
-export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
+import { Button } from '@carbon/react';
+import LandingPage from './home/page';
 
-  )
+export default function Page() {
+  return <LandingPage />;
 }
