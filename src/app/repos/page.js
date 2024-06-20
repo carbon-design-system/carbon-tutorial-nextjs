4caebdf90f2b1bd12a0b495879a9977f19b6a60f
Ivan Warlock <Ivan.Warlock@ibm.com>
2024-06-19 18:26:46 -0600

    feat(tutorial): complete step 3

--- src/app/repos/page.js
+++ src/app/repos/page.js
@@ -1,8 +1,17 @@
 'use client';
 
 import RepoTable from './RepoTable';
-import { Column, Grid } from '@carbon/react';
+import {
+  Link,
+  DataTableSkeleton,
+  Pagination,
+  Grid,
+  Column,
+} from '@carbon/react';
+import React, { useEffect, useState } from 'react';
+import { Octokit } from '@octokit/core';
 
+const octokitClient = new Octokit({});
 const headers = [
   {
     key: 'name',
@@ -29,6 +38,31 @@ const headers = [
     header: 'Links',
   },
 ];
+
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+const getRowItems = (rows) =>
+  rows.map((row) => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers_count,
+    issueCount: row.open_issues_count,
+    createdAt: new Date(row.created_at).toLocaleDateString(),
+    updatedAt: new Date(row.updated_at).toLocaleDateString(),
+    links: <LinkList url={row.html_url} homepageUrl={row.homepage} />,
+  }));
 
 const rows = [
   {
@@ -61,10 +95,72 @@ const rows = [
 ];
 
 function RepoPage() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [rows, setRows] = useState([]);
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
+
+  useEffect(() => {
+    async function getCarbonRepos() {
+      const res = await octokitClient.request('GET /orgs/{org}/repos', {
+        org: 'carbon-design-system',
+        per_page: 75,
+        sort: 'updated',
+        direction: 'desc',
+      });
+
+      if (res.status === 200) {
+        setRows(getRowItems(res.data));
+      } else {
+        setError('Error obtaining repository data');
+      }
+      setLoading(false);
+    }
+
+    getCarbonRepos();
+  }, []);
+
+  if (loading) {
+    return (
+      <Grid className="repo-page">
+        <Column lg={16} md={8} sm={4} className="repo-page__r1">
+          <DataTableSkeleton
+            columnCount={headers.length + 1}
+            rowCount={10}
+            headers={headers}
+          />
+        </Column>
+      </Grid>
+    );
+  }
+
+  if (error) {
+    return `Error! ${error}`;
+  }
+
+  // If we're here, we've got our data!
   return (
     <Grid className="repo-page">
       <Column lg={16} md={8} sm={4} className="repo-page__r1">
-        <RepoTable headers={headers} rows={rows} />
+        <RepoTable
+          headers={headers}
+          rows={rows.slice(firstRowIndex, firstRowIndex + currentPageSize)}
+        />
+        <Pagination
+          totalItems={rows.length}
+          backwardText="Previous page"
+          forwardText="Next page"
+          pageSize={currentPageSize}
+          pageSizes={[5, 10, 15, 25]}
+          itemsPerPageText="Items per page"
+          onChange={({ page, pageSize }) => {
+            if (pageSize !== currentPageSize) {
+              setCurrentPageSize(pageSize);
+            }
+            setFirstRowIndex(pageSize * (page - 1));
+          }}
+        />
       </Column>
     </Grid>
   );
